3eddf17ed8002baca77b633177f970ebcfed003c
Esther Siram <isusecalea@yahoo.com>
2017-01-27 15:32:40 -0600

    First commit

---
+++ imalive/schema.sql
@@ -0,0 +1,15 @@
+drop table if exists table_survivors;
+create table table_survivors (
+  id integer primary key autoincrement,
+  familyName text not null,
+  personalName text not null,
+  nickname text not null,
+  sex text not null,
+  age integer not null,
+  originCountry text not null,
+  originCity text not null,
+  other text not null,
+  todayDate timestamp,
+  username text not null,
+  password text not null
+);
